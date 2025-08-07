f88aa7dc32b644aa853afc2e04abcd9f0ae6787d
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-08-08 00:04:30 +0530

    feat : Admin dashboard subscriptin stats data fetching updated and tested

--- src/interface/admin/adminDashboard.controller.ts
+++ src/interface/admin/adminDashboard.controller.ts
@@ -4,21 +4,25 @@ import { UserRepositoryImpl } from "../../infrastructure/database/user/user.repo
 import { PaymentRepositoryImpl } from "../../infrastructure/database/payment/payment.repository.impl";
 import { BookingRepositoryImpl } from "../../infrastructure/database/booking/booking.repository.impl";
 import { ProviderRepositoryImpl } from "../../infrastructure/database/provider/provider.repository.impl";
-import { AdminFetchDashboardProviderStatsDataUseCase, AdminFetchDashboardTodaysDataUseCase, AdminFetchDashboardUserStatsDataUseCase } from "../../application/admin-use.case/adminDashboard.use-case";
+import { AdminFetchDashboardProviderStatsDataUseCase, AdminFetchDashboardSubscriptionStatsDataUseCase, AdminFetchDashboardTodaysDataUseCase, AdminFetchDashboardUserStatsDataUseCase } from "../../application/admin-use.case/adminDashboard.use-case";
+import { SubscriptionRepositoryImpl } from "../../infrastructure/database/subscription/subscription.repository.impl";
 
 const userRepositoryImpl = new UserRepositoryImpl();
 const providerRepositoryImpl = new ProviderRepositoryImpl();
 const paymentRepositoryImpl = new PaymentRepositoryImpl();
 const bookingRepositoryImpl = new BookingRepositoryImpl();
+const subscriptionRepositoryImpl = new SubscriptionRepositoryImpl();
 const adminFetchDashboardUserStatsDataUseCase = new AdminFetchDashboardUserStatsDataUseCase(userRepositoryImpl);
 const adminFetchDashboardTodaysDataUseCase = new AdminFetchDashboardTodaysDataUseCase(userRepositoryImpl, providerRepositoryImpl, paymentRepositoryImpl, bookingRepositoryImpl);
 const adminFetchDashboardProviderStatsDataUseCase = new AdminFetchDashboardProviderStatsDataUseCase( providerRepositoryImpl );
+const adminFetchDashboardSubscriptionStatsDataUseCase = new AdminFetchDashboardSubscriptionStatsDataUseCase( subscriptionRepositoryImpl );
 
 export class AdminDashboardController {
     constructor(
         private adminFetchDashboardTodaysDataUseCase: AdminFetchDashboardTodaysDataUseCase,
         private adminFetchDashboardUserStatsDataUseCase: AdminFetchDashboardUserStatsDataUseCase,
         private adminFetchDashboardProviderStatsDataUseCase: AdminFetchDashboardProviderStatsDataUseCase,
+        private adminFetchDashboardSubscriptionStatsDataUseCase: AdminFetchDashboardSubscriptionStatsDataUseCase,
     ) {
         this.fetchTodaysData = this.fetchTodaysData.bind(this);
         this.fetchUserStats = this.fetchUserStats.bind(this);
@@ -55,7 +59,7 @@ export class AdminDashboardController {
 
     async fetchSubscriptionStats(req: Request, res: Response) {
         try {
-            const result = await this.adminFetchDashboardProviderStatsDataUseCase.execute();
+            const result = await this.adminFetchDashboardSubscriptionStatsDataUseCase.execute();
             res.status(200).json(result);
         } catch (error) {
             HandleError.handle(error, res);
@@ -64,5 +68,5 @@ export class AdminDashboardController {
 
 }
 
-const adminDashboardController = new AdminDashboardController(adminFetchDashboardTodaysDataUseCase, adminFetchDashboardUserStatsDataUseCase, adminFetchDashboardProviderStatsDataUseCase );
+const adminDashboardController = new AdminDashboardController(adminFetchDashboardTodaysDataUseCase, adminFetchDashboardUserStatsDataUseCase, adminFetchDashboardProviderStatsDataUseCase, adminFetchDashboardSubscriptionStatsDataUseCase );
 export { adminDashboardController }
